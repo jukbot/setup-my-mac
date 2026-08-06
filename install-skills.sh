8be1e477e07f5a89732215113ebb833293f3f54a
Juk - Chukkrit V. <chukkrit.juk@gmail.com>
2026-08-06 23:52:05 +0700

    Update install-skills.sh

--- install-skills.sh
+++ install-skills.sh
@@ -4,33 +4,84 @@
 set -e
 
 # ─────────────────────────────────────────────
+# 🔦 MCP
+# ─────────────────────────────────────────────
+
+echo "🤖 Installing AI MCP..."
+
+claude mcp add browser-use -- uvx -y browser-use
+claude mcp add --transport http github https://api.githubcopilot.com/mcp/ # https://docs.github.com/en/copilot/how-tos/provide-context/use-mcp-in-your-ide/use-the-github-mcp-server
+claude mcp add --transport http cloudflare https://docs.mcp.cloudflare.com/mcp 
+
+# ─────────────────────────────────────────────
 # 🪄 Skills
 # ─────────────────────────────────────────────
 
 echo "🤖 Installing AI skills..."
-claude plugin marketplace add JuliusBrussee/caveman && claude plugin install caveman@caveman
-claude plugin marketplace add mattpocock/skills && claude plugin install mattpocock-skills@mattpococ
-claude plugin marketplace add pbakaus/impeccable && claude plugin install pbakaus/impeccable
+
+# ChromeDevTool MCP + Skills
 claude plugin marketplace add ChromeDevTools/chrome-devtools-mcp && claude plugin install chrome-devtools-mcp@chrome-devtools-plugins
-claude plugin marketplace add hyperb1iss/hyperskills && claude plugin install hyperskills@hyperb1iss
+
+# Caveman (Token Optimizing)
+claude plugin marketplace add JuliusBrussee/caveman && claude plugin install caveman@caveman
+
+# Temporal
+claude plugin marketplace add temporalio/claude-temporal-plugin && claude plugin install temporal@temporal-marketplace
+
+# Cloudflare (https://developers.cloudflare.com/agents/model-context-protocol/cloudflare/servers-for-cloudflare/)
 claude plugin marketplace add cloudflare/skills && claude plugin install cloudflare@cloudflare
+
+# Redis
+claude plugin marketplace add redis/agent-skills && claude install redis-development@redis
+
+# CodeRabbit
+claude plugin install coderabbit # https://docs.coderabbit.ai/cli/claude-code-integration
+
+# Productivity
+claude plugin marketplace add hyperb1iss/hyperskills && claude plugin install hyperskills@hyperb1iss
+claude plugin marketplace add mattpocock/skills && claude plugin install mattpocock-skills@mattpococ
 claude plugin marketplace add openai/codex-plugin-cc && claude plugin install codex@openai-codex
-claude plugin marketplace add obra/superpowers && claude plugin install superpowers@claude-plugins-official 
 
-npx add-skill vercel-labs/agent-skills
-npx impeccable install --providers=claude --scope=global
-npx skills add nutlope/hallmark --skill hallmark --agent claude-code -g
-npx skills add impeccable --agent claude-code -g
-npx skills add nuxt/ui --agent claude-code -g
-npx skills add vuejs-ai/skills -agent claude-code -g
-npx skills add kunchenguid/chrome-devtools-axi --agent claude-code -g
+# Langchain
+claude plugin marketplace add langchain-ai/langchain-skills && claude plugin install langchain-skills@langchain-skills
 
-# ─────────────────────────────────────────────
-# 🪄 MCP
-# ─────────────────────────────────────────────
+# Azure
+claude plugin install azure@claude-plugins-official
 
-echo "🤖 Installing AI MCP..."
-claude mcp add chrome-devtools --scope user npx chrome-devtools-mcp@latest
+# Claude Cowork
+claude plugin marketplace add anthropics/knowledge-work-plugins
+
+# Impeccable Design
+claude plugin marketplace add pbakaus/impeccable && 
+
+# Dotnet
+claude plugin marketplace add dotnet/skills
+
+# Productivity/Code-review
+npx skills add thananon/9arm-skills --agent claude-code --skill '*' --yes --global
+
+# Vercel
+npx skills add vercel-labs/agent-skills --agent claude-code --skill '*' --yes --global
+
+# Resend
+npx skills add resend/resend-skills --agent claude-code --skill '*' --yes --global
+
+# UX/UI Design
+claude plugin marketplace add pbakaus/impeccable
+npx skills add nutlope/hallmark --skill hallmark --agent claude-code --yes --global
+
+# Frontend Programming
+npx skills add nuxt/ui --agent claude-code --yes --global
+npx skills add vuejs-ai/skills --agent claude-code --yes --global
+
+# Backend Programming
+npx skills add samber/cc-skills-golang --agent claude-code --yes --global
+npx skills add openai/skills --skill aspnet-core --agent claude-code --yes --global
+
+# Agent tooling
+npx -y skills add kunchenguid/chrome-devtools-axi --agent claude-code --yes --global
+
+
 
 
 # Run Homebrew doctor to detect potential issues
